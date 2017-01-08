ef3bbdbc31387fb9c23664eb38f9c4cba005cd5f
Steve Losh <steve@stevelosh.com>
2017-01-08 12:25:16 +0000

    Fix ruin gen, add some flavor

--- src/entities/ruin.lisp
+++ src/entities/ruin.lisp
@@ -3,6 +3,40 @@
 (defparameter *venues*
   (read-file-into-form "data/venues.lisp"))
 
+(defparameter *graffiti*
+  #("give up"
+    "why me?"
+    "tom <3 alice"
+    "dave was here"
+    "420"
+    "goodbye"
+    "5823"
+    "jesus saves"
+    "your next"
+    "hail satan"
+    "run"
+    "head north"
+    "head south"
+    "were damned"
+    "hope is lost"
+    "turn back"))
+
+(defparameter *ruin-flavor*
+  #("Maybe there's something useful left."
+    "You're surprised it's still standing."
+    "Maybe it's worth taking a look."
+    "Perhaps you should scavenge?"
+    "You have a bad feeling about it."
+    "It brings back fond memories."
+    "The walls are crumbling."
+    "Sickly-looking leaves cover its walls."
+    "It looks like someone has been here recently."
+    "The walls are charred from a fire."
+    "A small animal scurries into a hold under the wall."
+    "Your parents used to live near one of these."
+    "The world has suddenly gotten quiet."
+    "A remnant of a happier time."))
+
 (define-entity ruin (coords trigger))
 
 (defun random-ruin-text ()
@@ -10,27 +44,8 @@
           (random-elt *venues*)
           (if (randomp ap::*graffiti-chance*)
             (format nil "Someone has graffitied \"~A\" on the wall."
-                    (string-upcase (random-elt #("give up"
-                                                 "why me?"
-                                                 "tom <3 alice"
-                                                 "dave was here"
-                                                 "420"
-                                                 "goodbye"
-                                                 "5823"
-                                                 "jesus saves"
-                                                 "hail satan"
-                                                 "run"
-                                                 "head north"
-                                                 "head south"
-                                                 "were damned"
-                                                 "hope is lost"
-                                                 "turn back"))))
-            (random-elt #("Maybe there's something useful left."
-                          "Perhaps you should scavenge?"
-                          "It brings back fond memories."
-                          "Your parents used to live near one of these."
-                          "The world has suddenly gotten quiet."
-                          "A remnant of a happier time.")))))
+                    (string-upcase (random-elt *graffiti*)))
+            (random-elt *ruin-flavor*))))
 
 (defun make-ruin (x y)
   (create-entity 'ruin
